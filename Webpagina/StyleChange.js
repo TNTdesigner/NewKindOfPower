5b164d0ce91222f3ea0cf891677e4000d2f166a1
BrentRuytjens <ruytjens.brent@gmail.com>
2017-08-11 13:16:12 +0200

    Meerdere stijlen
    heb hier en daar eens wat zitten prutsen met bepaalde stijlen voor zowel
    de gallery als voor de navigatiebalk. heb de oude wel behouden ;)

--- Webpagina/StyleChange.js
+++ Webpagina/StyleChange.js
@@ -1,8 +1,9 @@
-document.getElementById("style1").onclick = StyleChange1;
-document.getElementById("style2").onclick = StyleChange2;
+document.getElementById("style1").onclick = StyleChange1();
+document.getElementById("style2").onclick = StyleChange2();
 
 function StyleChange1() {
     document.getElementById("stylesheet")[0].setAttribute("href", "style2.css");
+
 }
 function StyleChange2() {
     document.getElementById("stylesheet")[0].setAttribute("href", "style.css");
